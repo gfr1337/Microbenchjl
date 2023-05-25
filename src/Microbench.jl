739f0aa33def71c2ed06bbfa34e82dd42101bfd6
George Rogers <george.f.rogers@gmail.com>
2023-05-25 14:55:09 -0400

    Update

--- src/Microbench.jl
+++ src/Microbench.jl
@@ -1,24 +1,38 @@
 #!/usr/bin/env -S julia --startup-file=no --project=@
 module Microbench
+using Base.Iterators
 using Printf
 
-function pigen(s, e)
-    res = zero(Float64)
-    for i = s:e
-        t = Int128(i)
-        res += 1/(t*t)
+
+function computepi²(e)
+    s = 0.0
+    for i = 1:e
+        t = Float64(i)
+        s += 1.0/(t*t)
     end
-    sqrt(6.0res)
+    6.0*s
 end
 
-function main()
-    for i = 1:100
-        @printf("%40.37f\n", pigen(1, 30_000_000))
+computepi(e) = sqrt(computepi²(e))
+
+struct Program
+    n::Int
+end
+
+function run(p::Program)
+    for i = 1:1000
+        @printf("%40.37f\n", computepi(30_000_000))
+    end
+end
+
+function main(args::String...)
+    for arg = args
+        run(Program(parse(Int, arg)))
     end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__()
-    main()
+    main(ARGS...)
 end
 
 end # module Microbench
