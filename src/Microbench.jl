7395232ba68113bb170782c9bea4c7599dc146fe
George Rogers <george.f.rogers@gmail.com>
2023-05-25 15:59:24 -0400

    update

--- src/Microbench.jl
+++ src/Microbench.jl
@@ -3,36 +3,33 @@ module Microbench
 using Base.Iterators
 using Printf
 
-
-function computepi²(e)
-    s = 0.0
-    for i = 1:e
-        t = Float64(i)
-        s += 1.0/(t*t)
-    end
-    6.0*s
-end
-
-computepi(e) = sqrt(computepi²(e))
-
 struct Program
-    n::Int
+    e::Float64
 end
 
-function run(p::Program)
-    for i = 1:1000
-        @printf("%40.37f\n", computepi(30_000_000))
-    end
+function value(p::Program)
+    inv(p.e*p.e)
 end
 
-function main(args::String...)
-    for arg = args
-        run(Program(parse(Int, arg)))
+value(ps) = sqrt(6.0mapreduce(value, +, ps, init=0.0))
+
+function run(p)
+    @printf("%40.37f\n", value(p))
+end
+
+function main(args)
+    e = parse(Int, args[2])
+    n = parse(Int, args[1])
+    for j = 1:n
+        @timev begin
+            programs = (Program(i) for i = 1:e)
+            run(programs)
+        end
     end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__()
-    main(ARGS...)
+    main(ARGS[:])
 end
 
 end # module Microbench
