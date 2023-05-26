41d732bacfc94e076604dd1b7d6e2d733b7c8d9c
George Rogers <george.f.rogers@gmail.com>
2023-05-26 07:16:30 -0400

    update

--- src/terms.jl
+++ src/terms.jl
@@ -5,6 +5,7 @@ export Term, inputtype, input, valuetype, value, run
 
 @traitdef Term{T}
 
+
 @traitfn inputtype(p::T) where {T; Term{T}} = inputtype(T)
 @traitfn valuetype(p::T) where {T; Term{T}} = valuetype(T)
 function input end
