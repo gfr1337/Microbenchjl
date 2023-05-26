77227eadb109456d3e6cd73cc0ec88a11586e186
George Rogers <george.f.rogers@gmail.com>
2023-05-25 21:01:40 -0400

    update

--- src/Microbench.jl
+++ src/Microbench.jl
@@ -3,13 +3,20 @@ module Microbench
 using Base.Iterators
 using Printf
 
-mutable struct PiTerm
-    e::Float64
+abstract type Term{I}
 end
 
-function piterm(p::PiTerm)
-    inv(p.e*p.e)
+input(p::Term) = p.input
+
+value(p::Term) = p.input
+
+struct PiTerm <: Term{Float64}
+    input::Float64
 end
+
+input(p::PiTerm) = p.input
+
+value(p::PiTerm) = inv(input(p) * input(p))
 
 function piterms(n::Int64)
     res = Vector{PiTerm}(undef, n)
@@ -19,24 +26,39 @@ function piterms(n::Int64)
     res
 end
 
-pisum(ps) = sqrt(6.0mapreduce(piterm, +, ps, init=0.0))
+pisum²(ps) = 6.0mapreduce(value, +, ps, init=0.0)
 
-pisum(n::Integer) = piterms(n) |> pisum
+pisum²(n::Integer) = piterms(n) |> pisum²
+
+pisum(n) = pisum²(n) |> sqrt
+
+struct Program <: Term{Int}
+    input::Int
+end
+
+input(p::Program) = p.input
+
+value(p::Program) = input(p) |> pisum
+
 
 function run(p)
-    @printf("%40.37f\n", pisum(p))
+    @printf("%40.37f\n", value(p))
 end
 
 function main(n, e)
     let n = parse(Int, n), e = parse(Int, e)
+        programs = Vector{Program}(undef, n)
         for i = 1:n
-            run(e)
+            programs[i] = Program(e)
+        end
+        for p = programs
+            run(p)
         end
     end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__()
-    @time main(ARGS...)
+    main(ARGS...)
 end
 
 end # module Microbench
