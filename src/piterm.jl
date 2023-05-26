a2e934dbd96d2d34991f51893b824f745f8bf1db
George Rogers <george.f.rogers@gmail.com>
2023-05-26 12:33:25 -0400

    update

--- src/piterm.jl
+++ src/piterm.jl
@@ -12,10 +12,10 @@ Terms.inputtype(::Type{PiTerm}) = Int
 Terms.valuetype(::Type{PiTerm}) = Float64
 Terms.value(p::PiTerm) = inv(input(p)*input(p))
 
-piterms() = (PiTerm(i) for i = Iterators.countfrom(1))
-piterms(n) = (PiTerm(i) for i = 1:n)
+piterms() = piterms(Iterators.countfrom(1))
+piterms(r) = (PiTerm(i) for i = r)
 
-pisumsq(n) = 6.0*mapreduce(value, +, piterms(n), init=0.0)
+pisumsq(n) = 6.0*mapreduce(value, +, piterms(1:n), init=0.0)
 
 pisum(n) = pisumsq(n) |> sqrt
 
