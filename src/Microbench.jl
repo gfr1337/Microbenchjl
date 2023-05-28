fc584b0d66fb010b741b9ae94df9a23208466f37
George Rogers <george.f.rogers@gmail.com>
2023-05-27 21:26:07 -0400

    update

--- src/Microbench.jl
+++ src/Microbench.jl
@@ -8,14 +8,14 @@ include("piterm.jl")
 
 using .PiTerms
 
-function main(n=10, e=30000000)
+function main(n=10, e=30000000)::Nothing
     println("Julia")
     for i = 1:n
         benchmark(100, e)
     end
 end
 
-function benchmark(n, e)
+function benchmark(n, e)::Nothing
     t = @timed for i = 1:n
         v = computepi(e)
         @printf("%16.9f\n", v)
