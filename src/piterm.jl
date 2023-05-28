4bb58fba481cbc25856b2e0b98982c489ace15ab
George Rogers <george.f.rogers@gmail.com>
2023-05-28 17:45:54 -0400

    update

--- src/piterm.jl
+++ src/piterm.jl
@@ -1,7 +1,7 @@
 module PiTerm
 export computepi, computepi2, computepi²
 
-@inline function _powx(x::N, n::Integer)::N where N <: Real
+@inline @fastmath function _powx(x::N, n::Integer)::N where N <: Real
     acc = one(x)
     @simd for _ = 1:n
         acc *= x
@@ -9,8 +9,8 @@ export computepi, computepi2, computepi²
     acc
 end
 
-@inline pow(x::Integer, n::Integer)::Float64 = pow(Float64(x), n)
-@inline function pow(x::F, n::Integer)::F where F <: Union{AbstractFloat, Rational}
+@fastmath @inline pow(x::Integer, n::Integer)::Float64 = @inline pow(Float64(x), n)
+@fastmath @inline function pow(x::F, n::Integer)::F where F <: Union{AbstractFloat, Rational}
     if n < 0
         @inline _powx(inv(x), -n)
     else
@@ -18,7 +18,7 @@ end
     end
 end
 
-@fastmath function computepi2(n::AbstractRange{<:Integer})::Float64
+@inline @fastmath function computepi2(n::AbstractRange{<:Integer})::Float64
     s = 0.0
     @simd for i = n
         s += pow(i, -2)
@@ -26,11 +26,11 @@ end
     6.0*s
 end
 
-computepi2(n::Integer)::Float64 = computepi2(1:n)
+@inline computepi2(n::Integer)::Float64 = computepi2(1:n)
 
 const computepi² = computepi2
 
-@fastmath computepi(ts)::Float64 = sqrt(computepi2(ts))
+@inline @fastmath computepi(ts)::Float64 = sqrt(computepi2(ts))
 
 precompile(computepi2, (Int,))
 
