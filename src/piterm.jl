ba63767d42b3edbb27b39b2ec55bb474cdd62594
George Rogers <george.f.rogers@gmail.com>
2023-05-28 14:07:47 -0400

    update

--- src/piterm.jl
+++ src/piterm.jl
@@ -1,10 +1,20 @@
 using Printf
 
+function pow(t::N, p::Integer)::N where N <: Number
+    if p < 0 
+        return pow(inv(t), -p)
+    end
+    acc = one(t)
+    @simd for _ = 1:p
+        acc *= t
+    end
+    acc
+end
+
 @fastmath function computepi2(n::AbstractRange{<:Integer})::Float64
     s = 0.0
     for i = n
-        t = inv(i)
-        s += t*t
+        s += pow(inv(i), 2)
     end
     6.0*s
 end
