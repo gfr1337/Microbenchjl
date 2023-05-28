67a4072f1471047979daa11256dc8f1b51fbd05b
George Rogers <george.f.rogers@gmail.com>
2023-05-28 13:05:59 -0400

    update

--- src/piterm.jl
+++ src/piterm.jl
@@ -1,14 +1,18 @@
 using Printf
 
-@fastmath function computepi²(n::Integer)::Float64
+@fastmath function computepi2(n::AbstractRange{Int})::Float64
     s = 0.0
-    for i = 1:n
+    for i = n
         t = inv(i)
         s += t*t
     end
     6.0*s
 end
 
-@fastmath computepi(ts::Integer)::Float64 = sqrt(computepi²(ts))
+computepi2(n::Int)::Float64 = computepi2(1:n)
+
+const computepi² = computepi2
+
+@fastmath computepi(ts)::Float64 = sqrt(computepi2(ts))
 
 precompile(computepi², (Int,))
