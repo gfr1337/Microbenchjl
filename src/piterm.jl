a6d3d6c07195d7a88b1eb0bf4c7e45330f371dae
George Rogers <george.f.rogers@gmail.com>
2023-05-28 18:12:29 -0400

    update

--- src/piterm.jl
+++ src/piterm.jl
@@ -1,20 +1,25 @@
 module PiTerm
 export computepi, computepi2, computepi²
 
-@inline @fastmath function _powx(x::N, n::Integer)::N where N <: Real
-    acc = one(x)
-    @simd for _ = 1:n
-        acc *= x
-    end
-    acc
-end
+begin
+    local _powx
+    global pow
 
-@fastmath @inline pow(x::Integer, n::Integer)::Float64 = @inline pow(Float64(x), n)
-@fastmath @inline function pow(x::F, n::Integer)::F where F <: Union{AbstractFloat, Rational}
-    if n < 0
-        @inline _powx(inv(x), -n)
-    else
-        @inline _powx(Float64(x), n)
+    @inline @fastmath function _powx(x::N, n::Integer)::N where N <: Real
+        acc = one(x)
+        @simd for _ = 1:n
+            acc *= x
+        end
+        acc
+    end
+
+    @fastmath @inline pow(x::Integer, n::Integer)::Float64 = @inline pow(Float64(x), n)
+    @fastmath @inline function pow(x::F, n::Integer)::F where F <: Union{AbstractFloat, Rational}
+        if n < 0
+            @inline _powx(inv(x), -n)
+        else
+            @inline _powx(x, n)
+        end
     end
 end
 
